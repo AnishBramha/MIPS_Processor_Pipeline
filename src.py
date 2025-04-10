d4d6aaffaa473bc2b451b605719570dfd13c11f4
Akshay-K-M <akshaykmfilms@gmail.com>
2025-04-10 18:14:51 +0530

    working on instruction decoding

--- src.py
+++ src.py
@@ -36,3 +36,88 @@ class Processor:
         else:
             self.if_id['ins'] = None
             self.if_id['pc'] = self.pc
+        
+    def id_stage(self):
+        if not self.if_id['ins']:
+            self.id_ex['ins']=None
+            return
+        ins=self.if_id['ins']
+        self.id_ex['ins']=ins
+        self.id_ex['pc']=self.if_id['pc']
+        parts=ins.replace(',','').split()
+        op=parts[0]
+        self.id_ex['control']={
+            'regwrite':0,
+            'memread':0,
+            'memwrite':0,
+            'branch':0,
+            'jump':0,
+            'alusrc':0,
+            'regdst':0,
+            'aluop':'add'
+        }
+        if op=='add':
+            self.id_ex['rd']=int(parts[1][1:])
+            self.id_ex['rs']=int(parts[2][1:])
+            self.id_ex['rt']=int(parts[3][1:])
+            self.id_ex['control'].update({
+                'regwrite':1,
+                'regdst':1,
+                'aluop':'add'
+            })
+        elif op=='addi':
+            self.id_ex['rt']=int(parts[1][1:])
+            self.id_ex['rs']=int(parts[2][1:])
+            self.id_ex['imm']=int(parts[3])
+            self.id_ex['control'].update({
+                'regwrite':1,
+                'alusrc':1,
+                'aluop':'add'
+            })
+        elif op=='lw':
+            rt = parts[1][1:]
+            offset_rs = parts[2].split('(')
+            self.id_ex['rt'] = int(rt)
+            self.id_ex['imm'] = int(offset_rs[0])
+            self.id_ex['rs'] = int(offset_rs[1][1:-1])  
+            self.id_ex['control'].update({
+                'regwrite': 1,
+                'memread': 1,
+                'alusrc': 1,
+                'aluop': 'add'
+            })
+            self.mem_latency = random.choice([2, 3]) 
+        elif op=='sw':
+            rt=parts[1][1:]
+            offset_rs = parts[2].split('(')
+            self.id_ex['rt'] = int(rt)
+            self.id_ex['imm'] = int(offset_rs[0])
+            self.id_ex['rs'] = int(offset_rs[1][1:-1])
+            self.id_ex['control'].update({
+                'memwrite': 1,
+                'alusrc': 1,
+                'aluop': 'add'
+            })
+            self.mem_latency = random.choice([2, 3])
+        elif op == 'beq':
+            self.id_ex['rs'] = int(parts[1][1:])
+            self.id_ex['rt'] = int(parts[2][1:])
+            self.id_ex['imm'] = int(parts[3])
+            self.id_ex['control'].update({
+                'branch': 1,
+                'aluop': 'sub'  
+            })
+        elif op=='j':
+            self.id_ex['imm'] = int(parts[1])
+            self.id_ex['control']['jump'] = 1
+        elif op == 'subi':
+            self.id_ex['rt'] = int(parts[1][1:])
+            self.id_ex['rs'] = int(parts[2][1:])
+            self.id_ex['imm'] = int(parts[3])
+            self.id_ex['control'].update({
+                'regwrite': 1,
+                'alusrc': 1,
+                'aluop': 'sub'
+            })
+        elif op == 'halt':
+            pass
