3e6962a9afd18e108b300f2deabfd2041ddfd9df
Anish Teja Bramhajosyula <Anish.Teja@iiitb.ac.in>
2025-04-10 18:24:01 +0530

    Something

--- src.py
+++ src.py
@@ -36,3 +36,59 @@ class Processor:
         else:
             self.if_id['ins'] = None
             self.if_id['pc'] = self.pc
+
+
+    def ex_stage(self) -> None:
+
+        if not self.id_ex['ins'] == None:
+            
+            self.ex_mem['ins'] = None
+            return
+        
+        ins = self.id_ex['ins']
+
+        self.ex_mem['ins'] = ins
+        self.ex_mem['pc'] = self.id_ex['pc']
+        self.ex_mem['control'] = self.id_ex['control']
+
+        rs_val = self.regs[self.id_ex['rs']] if self.id_ex['rs'] is not None else 0
+        rt_val = self.regs[self.id_ex['rt']] if self.id_ex['rt'] is not None else 0
+
+        if self.id_ex['control']['alusrc']:
+            op2 = self.id_ex['imm']
+
+        else:
+            op2 = rt_val
+
+        if self.id_ex['control']['aluop'] == 'add':
+            self.ex_mem['alu_res'] = rs_val + op2
+
+        elif self.id_ex['control']['aluop'] == 'sub':
+            self.ex_mem['alu_res'] = rs_val - op2
+
+        self.ex_mem['rt_data'] = rt_val
+        self.ex_mem['rd'] = self.id_ex['rd'] if self.id_ex['rd'] is not None else self.id_ex['rt']
+
+        if self.id_ex['control']['branch']:
+
+            if self.ex_mem['alu_res'] == 0:
+
+                self.pc = self.id_ex['pc'] + 4 + (self.id_ex['imm'] << 2)
+                self.branch_taken = True
+
+                pc_next = self.id_ex['pc'] + 4
+
+                if pc_next in self.ins_mem and not self.ins_mem[pc_next].startswith('nop'):
+                    self.delay_slot_used += 1
+
+                else:
+                    self.delay_slot_wasted += 1
+
+        if self.id_ex['control']['memread'] and ((self.id_ex['rs'] is not None and self.id_ex['rs'] == self.ex_mem['rd']) or (self.id_ex['rt'] is not None and self.id_ex['rt'] == self.id_ex['rd'])):
+
+            self.stall = True
+            self.stall_count += 1
+            self.load_stalls += 1
+
+        
+
